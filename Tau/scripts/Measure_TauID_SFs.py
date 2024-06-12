218a86ecf2839d9f0f73861737ccc1a54fc3130e
Jacopo Malvaso <jacopo.malvaso@stud.unifi.it>
2024-06-12 16:51:00 +0200

    Generalization of the scripts (you only have to change the baseFolder accordigly, baseFolder declaration is inside HighPT/Tau/python/utilsHighPT.py)

--- Tau/scripts/Measure_TauID_SFs.py
+++ Tau/scripts/Measure_TauID_SFs.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python3
 import os
-
+import HighPT.Tau.utilsHighPT as utils
 # Define the two sets of scripts
 scripts = [
     'FakeFactorHighPt.py',
     'DatacardsWToMuNu.py',
     'DatacardsWToTauNu.py',
-    '../combine/RunCombineHighPT.py'
+    '../combine/RunFitHighPT.py'
 ]
-
-scripts_pt_binned = [
-    'FakeFactorHighPt.py',
-    'DatacardsWToMuNu.py',
-    'DatacardsWToTauNu.py',
-    '../combine/RunCombineMultiDim.py'
-]
-
-# Ask the user which set of scripts they want to use
-user_choice = input("Which set of scripts do you want to use? Enter 1 for scripts or 2 for scripts_pt_binned: ")
-
-# Select the set of scripts based on the user's choice
-if user_choice == "1":
-    selected_scripts = scripts
-elif user_choice == "2":
-    selected_scripts = scripts_pt_binned
-else:
-    print("Invalid choice. Exiting.")
-    exit()
 
 # Execute the selected scripts
-for script in selected_scripts:
+for script in scripts:
     print(f"Running {script}...")
     user_input = input(f"Do you want to execute {script}? (y/n): ").lower()
     if user_input == 'n':
